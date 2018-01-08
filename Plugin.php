52ba17af8a22bcf49713911aeafeb13b2fcd6e2d
Luke Towers <luke@luketowers.ca>
2018-01-08 09:32:32 -0600

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,56 @@
+<?php namespace LukeTowers\MenuChangerExample;
+
+use Event;
+use Backend;
+use BackendMenu;
+use Backend\Controllers\Users;
+use System\Classes\PluginBase;
+
+/**
+ * UserMenu Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Menu Changer Example',
+            'description' => "Example plugin to demonstrate how to change a controller's backend menu location",
+            'author'      => 'LukeTowers',
+            'icon'        => 'icon-user'
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return [
+            'users' => [
+                'label'       => 'backend::lang.user.menu_label',
+                'url'         => Backend::url('backend/users'),
+                'icon'        => 'icon-user',
+                'permissions' => ['backend.manage_users'],
+            ],
+        ];
+    }
+
+    public function boot()
+    {
+        Event::listen('backend.page.beforeDisplay', function ($controller, $action, $params) {
+            if (!($controller instanceof UsersController)) {
+                return;
+            }
+
+            BackendMenu::setContext('LukeTowers.Users', 'users');
+        });
+    }
+}
